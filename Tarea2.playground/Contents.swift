e57bd2e9c651db978bfd8a4112494c9147e045a3
Rodrigo <rodrigohernandez777@gmail.com>
2016-10-02 14:33:24 -0500

    Se agrega mensaje de salida de la dupla

--- Tarea2.playground/Contents.swift
+++ Tarea2.playground/Contents.swift
@@ -20,19 +20,24 @@ class Auto{
     }
     func cambioDeVelocidad()->(actual: Int, velocidadEnCadena: String){
         var velocidadActual = velocidad
+        var mensajeVelocidadActual : String
         if(velocidad == Velocidades.Apagado){
+            mensajeVelocidadActual = "Apagado"
             velocidad = Velocidades.VelocidadBaja
         }
         else if(velocidad == Velocidades.VelocidadBaja){
+            mensajeVelocidadActual = "Velocidad baja"
             velocidad = Velocidades.VelocidadMedia
         }
         else if(velocidad == Velocidades.VelocidadMedia){
+            mensajeVelocidadActual = "Velocidad media"
             velocidad = Velocidades.VelocidadAlta
         }
         else{ // Si actual Velocidad es VelocidadAlta
+            mensajeVelocidadActual = "Velocidad alta"
             velocidad = Velocidades.VelocidadMedia
         }
-        return (velocidadActual.rawValue,"La velocidad actual es: \(velocidadActual.rawValue), estado: \(velocidadActual)")
+        return (velocidadActual.rawValue,"\(mensajeVelocidadActual)")
     }
     
 }
@@ -40,31 +45,31 @@ class Auto{
 var auto1 = Auto()
 print("Velocidad inicial: \(auto1.velocidad.rawValue), estado: \(auto1.velocidad)")
 for iterador in 1...20 {
-    print("Cambio de Velocidad #\(iterador) : \(auto1.cambioDeVelocidad().velocidadEnCadena)")
+    print(auto1.cambioDeVelocidad())
 }
 
 /* SALIDA EN CONSOLA:
 Velocidad inicial: 0, estado: Apagado
-Cambio de Velocidad #1 : La velocidad actual es: 0, estado: Apagado
-Cambio de Velocidad #2 : La velocidad actual es: 20, estado: VelocidadBaja
-Cambio de Velocidad #3 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #4 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #5 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #6 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #7 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #8 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #9 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #10 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #11 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #12 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #13 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #14 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #15 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #16 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #17 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #18 : La velocidad actual es: 120, estado: VelocidadAlta
-Cambio de Velocidad #19 : La velocidad actual es: 50, estado: VelocidadMedia
-Cambio de Velocidad #20 : La velocidad actual es: 120, estado: VelocidadAlta
+(0, "Apagado")
+(20, "Velocidad baja")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
+(50, "Velocidad media")
+(120, "Velocidad alta")
 */
 
 
